95fb67b7507755d0a2bd081f857bab7e1c38d39f
Lars Haalck <l_haal02@uni-muenster.de>
2016-09-07 19:18:35 +0200

    neues Diagramm für reprojection error bei Rotation

--- Matlab/degrees.m
+++ Matlab/degrees.m
@@ -1,16 +1,24 @@
 function [] = degrees()
 values = load('reprojByDeg.txt', '-ascii');
 
-s = size(values)
+s = size(values);
 
 for i = 1:(s(1)/2)
-    sx = values(i,:)
+    sx = values(i,:);
     sy = values(i+1,:);
     norms(:,i) = sqrt(sx.^2 + sy.^2);
     avgs(i) = mean(norms(:,i));
 end
 
-boxplot(norms, 'Labels',{'0', '1','2','3','4','5','6','7','8','9','10','11','12'});
+x = 0:1:12;
+h1 = bar(x,avgs);
+x = -10:1:22
+y = mean(avgs)*ones(1,22+10+1);
+hold on;
+h2 = plot(x,y,'m--', 'LineWidth', 2);
+axis([-1 13 0 3])
+legend([h2],{strcat('insg. Durschnitt:', num2str(mean(avgs)))});
+%boxplot(norms, 'Labels',{'0', '1','2','3','4','5','6','7','8','9','10','11','12'});
 
 
 % 
